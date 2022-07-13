5a181c6a9237536d2a62c19ff77f867280b3e74b
jusmccar <justinmccartney1@yahoo.com>
2022-07-12 20:22:40 -0700

    Add MongoDB

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
 const port = 3000;
 
 app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
 
-app.listen(port, console.log(`Server is listening on ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`Server is listening on ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();

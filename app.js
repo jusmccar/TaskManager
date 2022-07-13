9d1262c4690ac8f47d05006ae15ad9736008f462
jusmccar <justinmccartney1@yahoo.com>
2022-07-12 23:04:23 -0700

    Add not found page

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
+const notFound = require("./middleware/not-found");
 require("dotenv").config();
 
 const port = 3000;
@@ -10,6 +11,8 @@ app.use(express.static("./public"));
 app.use(express.json());
 
 app.use("/api/v1/tasks", tasks);
+
+app.use(notFound);
 
 const start = async () => {
   try {
